8925778f9fb3cc9aa72c8d75463322ab325e4796
supershuaifeng <limchinfeng@gmail.com>
2023-09-20 11:26:37 +0800

    folder setup

---
+++ app/(dashboard)/(routes)/page.tsx
@@ -0,0 +1,12 @@
+import { Button } from "@/components/ui/button";
+
+export default function Home() {
+  return (
+    <div className="text-3xl font-bold text-sky-700">
+      Hello World
+      <Button variant="destructive">
+        yoo
+      </Button>
+    </div>
+  )
+}
