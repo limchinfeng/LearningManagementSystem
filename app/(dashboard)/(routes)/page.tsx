9e5994fa1d97054dbfd7db5f97b6ab9540cc2f25
supershuaifeng <limchinfeng@gmail.com>
2023-09-20 14:48:12 +0800

    Done authentication using clerk

--- app/(dashboard)/(routes)/page.tsx
+++ app/(dashboard)/(routes)/page.tsx
@@ -1,12 +1,10 @@
 import { Button } from "@/components/ui/button";
+import { UserButton } from "@clerk/nextjs";
 
 export default function Home() {
   return (
-    <div className="text-3xl font-bold text-sky-700">
-      Hello World
-      <Button variant="destructive">
-        yoo
-      </Button>
+    <div className="">
+      <UserButton afterSignOutUrl="/"/>
     </div>
   )
 }
