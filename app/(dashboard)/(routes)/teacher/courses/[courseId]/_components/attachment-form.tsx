0f32bb7b4987efd8f499a01861a049a072baae99
supershuaifeng <limchinfeng@gmail.com>
2023-09-21 22:51:48 +0800

    Done prisma finalization

--- app/(dashboard)/(routes)/teacher/courses/[courseId]/_components/attachment-form.tsx
+++ app/(dashboard)/(routes)/teacher/courses/[courseId]/_components/attachment-form.tsx
@@ -56,7 +56,7 @@ export const AttachmentForm = ({
   return (
     <div className="mt-6 border bg-slate-100 rounded-md p-4">
       <div className="font-medium flex items-center justify-between">
-        Course Image
+        Course attachments
         <Button onClick={toggleEdit} variant="ghost"> 
           {isEditing && (
             <>Cancel</>
